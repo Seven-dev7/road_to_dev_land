a02962c7324492b56e7ba4f26676824d55da09d3
seven-dev7 <seven.tdr7@gmail.com>
2021-11-10 09:13:06 +0100

    add fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   end
 
   def search
-    return redirect_to root_path, alert: "Veuillez entrer un pseudo valide" if params[:nickname].present? || params[:nickname].blank?
+    return redirect_to root_path, alert: "Veuillez entrer un pseudo valide" if params[:nickname].present?
 
     @response_body = CodewarsService::Search.by_nickname(nickname: params[:nickname]).body
     @response_content = JSON.parse(@response_body)
